9915b5438c8ae6e879a88dfc3c54c4386f2f0e14
DORA_XIE <joy8963259@gmail.com>
2022-11-14 22:32:32 +0800

    RWD

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'home/mock_screen.dart';
 import 'kg_theme/kg_theme.dart';
 import 'modal/kg_theme_data.dart';
 
-import 'dart:convert' show Utf8Decoder, jsonDecode, utf8;
+import 'dart:convert' show Utf8Decoder, utf8;
 
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' show AnchorElement;
@@ -66,207 +66,205 @@ class _MyHomePageState extends State<MyHomePage> {
                 right: 10,
                 child: ChangeThemeBtn(),
               ),
-              Positioned(
-                top: 100,
-                left: 10,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          ref.read(kgThemeDataStateProvider.notifier).state =
-                              KgThemeData();
-                        },
-                        child: Icon(Icons.refresh,
-                            color: ref
-                                .watch(kgThemeDataStateProvider.notifier)
-                                .state
-                                .primary),
-                      ),
-                      ...[
-                        ThemeColorTypes.primary,
-                        ThemeColorTypes.primaryContainer,
-                        ThemeColorTypes.background,
-                        ThemeColorTypes.secondary,
-                        ThemeColorTypes.textColor,
-                      ].map(
-                        (type) => Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: ThemeControlScreen(
-                            colorType: type,
-                            kgProvider:
-                                ref.read(kgThemeDataStateProvider.notifier),
-                          ),
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            width: 100,
-                            padding: const EdgeInsets.only(right: 8.0),
-                            child: Text('BorderRadius',
-                                style: Theme.of(context).textTheme.headline5),
-                          ),
-                          Slider(
-                            max: 30,
-                            inactiveColor: ref
-                                .watch(kgThemeDataStateProvider.notifier)
-                                .state
-                                .primary
-                                .withOpacity(0.5),
-                            activeColor: ref
-                                .watch(kgThemeDataStateProvider.notifier)
-                                .state
-                                .primary,
-                            label: ref
-                                .watch(kgThemeDataStateProvider)
-                                .borderRadiusValue
-                                .toString(),
-                            value: ref
-                                .watch(kgThemeDataStateProvider)
-                                .borderRadiusValue,
-                            onChanged: (value) {
-                              final provider =
-                                  ref.read(kgThemeDataStateProvider.notifier);
-                              provider.state = provider.state.copyWith(
-                                borderRadiusValue: value,
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          MouseRegion(
-                            cursor: SystemMouseCursors.click,
-                            child: GestureDetector(
-                              onTap: () async {
-                                var picked =
-                                    await FilePicker.platform.pickFiles(
-                                  type: FileType.custom,
-                                  allowedExtensions: ['txt'],
-                                );
-                                if (picked != null) {
-                                  if (picked.files.first.bytes != null) {
-                                    final data = const Utf8Decoder()
-                                        .convert(picked.files.first.bytes!);
-                                    try {
-                                      List<String> str = data
-                                          .replaceAll("{", "")
-                                          .replaceAll("}", "")
-                                          .split(",");
-                                      Map<String, dynamic> result = {};
-                                      for (int i = 0; i < str.length; i++) {
-                                        List<String> s = str[i].split(":");
-                                        result.putIfAbsent(
-                                            s[0].trim(), () => s[1].trim());
-                                      }
-                                      result.forEach((key, value) {
-                                        final number = double.tryParse(value);
-                                        if (number != null) {
-                                          result[key] = number;
-                                        }
-                                      });
-                                      final themeData =
-                                          KgThemeData.fromJson(result);
-                                      ref
-                                          .read(
-                                              kgThemeDataStateProvider.notifier)
-                                          .state = themeData;
-                                    } catch (e) {
-                                      print('=======error : $e=========');
-                                    }
-                                  }
-                                }
-                              },
-                              child: Container(
-                                padding: KgThemeData().padding,
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    width: 3,
-                                    color:
-                                        Theme.of(context).colorScheme.primary,
-                                  ),
-                                  borderRadius: BorderRadius.all(
-                                    ref
-                                        .watch(kgThemeDataStateProvider)
-                                        .borderRadius,
-                                  ),
-                                ),
-                                child: Text(
-                                  'Import',
-                                  style: Theme.of(context).textTheme.headline3,
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 10),
-                          MouseRegion(
-                            cursor: SystemMouseCursors.click,
-                            child: GestureDetector(
-                              onTap: () {
-                                void saveTextFile(
-                                    String text, String filename) {
-                                  AnchorElement()
-                                    ..href =
-                                        '${Uri.dataFromString(text, mimeType: 'text/plain', encoding: utf8)}'
-                                    ..download = filename
-                                    ..style.display = 'none'
-                                    ..click();
-                                }
-
-                                final output =
-                                    ref.read(kgThemeDataStateProvider).toJson();
-                                output.remove('isDark');
-                                saveTextFile(
-                                    output.toString(), 'kg_theme_data');
-                              },
-                              child: Container(
-                                padding: KgThemeData().padding,
-                                decoration: BoxDecoration(
-                                  color: Theme.of(context).colorScheme.primary,
-                                  border: Border.all(
-                                    width: 3,
-                                    color:
-                                        Theme.of(context).colorScheme.primary,
-                                  ),
-                                  borderRadius: BorderRadius.all(
-                                    ref
-                                        .watch(kgThemeDataStateProvider)
-                                        .borderRadius,
-                                  ),
-                                ),
-                                child: Text(
-                                  'Export',
-                                  style: Theme.of(context).textTheme.headline3,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
+              if (MediaQuery.of(context).size.width > 500)
+                _buildThemeController(ref, context),
+              SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 100),
+                    const MockScreen(
+                      child: MockContent(),
+                    ),
+                    if (MediaQuery.of(context).size.width < 500)
+                      Container(
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.all(10),
+                          child: _buildThemeController(ref, context)),
+                  ],
                 ),
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  MockScreen(
-                    child: MockContent(),
-                  ),
-                ],
               ),
             ],
           ),
         );
       },
+    );
+  }
+
+  Widget _buildThemeController(WidgetRef ref, BuildContext context) {
+    return Positioned(
+      top: 100,
+      left: 10,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: () {
+                ref.read(kgThemeDataStateProvider.notifier).state =
+                    KgThemeData();
+              },
+              child: Icon(Icons.refresh,
+                  color: ref
+                      .watch(kgThemeDataStateProvider.notifier)
+                      .state
+                      .primary),
+            ),
+            ...[
+              ThemeColorTypes.primary,
+              ThemeColorTypes.primaryContainer,
+              ThemeColorTypes.background,
+              ThemeColorTypes.secondary,
+              ThemeColorTypes.textColor,
+            ].map(
+              (type) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ThemeControlScreen(
+                  colorType: type,
+                  kgProvider: ref.read(kgThemeDataStateProvider.notifier),
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Text('BorderRadius',
+                      style: Theme.of(context).textTheme.headline5),
+                ),
+                Slider(
+                  max: 30,
+                  inactiveColor: ref
+                      .watch(kgThemeDataStateProvider.notifier)
+                      .state
+                      .primary
+                      .withOpacity(0.5),
+                  activeColor: ref
+                      .watch(kgThemeDataStateProvider.notifier)
+                      .state
+                      .primary,
+                  label: ref
+                      .watch(kgThemeDataStateProvider)
+                      .borderRadiusValue
+                      .toString(),
+                  value: ref.watch(kgThemeDataStateProvider).borderRadiusValue,
+                  onChanged: (value) {
+                    final provider =
+                        ref.read(kgThemeDataStateProvider.notifier);
+                    provider.state = provider.state.copyWith(
+                      borderRadiusValue: value,
+                    );
+                  },
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    onTap: () async {
+                      var picked = await FilePicker.platform.pickFiles(
+                        type: FileType.custom,
+                        allowedExtensions: ['txt'],
+                      );
+                      if (picked != null) {
+                        if (picked.files.first.bytes != null) {
+                          final data = const Utf8Decoder()
+                              .convert(picked.files.first.bytes!);
+                          try {
+                            List<String> str = data
+                                .replaceAll("{", "")
+                                .replaceAll("}", "")
+                                .split(",");
+                            Map<String, dynamic> result = {};
+                            for (int i = 0; i < str.length; i++) {
+                              List<String> s = str[i].split(":");
+                              result.putIfAbsent(
+                                  s[0].trim(), () => s[1].trim());
+                            }
+                            result.forEach((key, value) {
+                              final number = double.tryParse(value);
+                              if (number != null) {
+                                result[key] = number;
+                              }
+                            });
+                            final themeData = KgThemeData.fromJson(result);
+                            ref.read(kgThemeDataStateProvider.notifier).state =
+                                themeData;
+                          } catch (e) {
+                            print('=======error : $e=========');
+                          }
+                        }
+                      }
+                    },
+                    child: Container(
+                      padding: KgThemeData().padding,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 3,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          ref.watch(kgThemeDataStateProvider).borderRadius,
+                        ),
+                      ),
+                      child: Text(
+                        'Import',
+                        style: Theme.of(context).textTheme.headline3,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    onTap: () {
+                      void saveTextFile(String text, String filename) {
+                        AnchorElement()
+                          ..href =
+                              '${Uri.dataFromString(text, mimeType: 'text/plain', encoding: utf8)}'
+                          ..download = filename
+                          ..style.display = 'none'
+                          ..click();
+                      }
+
+                      final output =
+                          ref.read(kgThemeDataStateProvider).toJson();
+                      output.remove('isDark');
+                      saveTextFile(output.toString(), 'kg_theme_data');
+                    },
+                    child: Container(
+                      padding: KgThemeData().padding,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        border: Border.all(
+                          width: 3,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          ref.watch(kgThemeDataStateProvider).borderRadius,
+                        ),
+                      ),
+                      child: Text(
+                        'Export',
+                        style: Theme.of(context).textTheme.headline3,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
